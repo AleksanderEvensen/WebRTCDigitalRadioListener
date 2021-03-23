343140697ff4c76d95ee5c82f974494e0858cd88
Aleksander Evensen <ahse03@gmail.com>
2021-03-23 13:17:25 +0100

    ADD(Base From layout done)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import styled from 'styled-components';
+
 import './App.css';
+import InputField from './components/InputField';
 
 interface AppProps {
     test?:string
 }
 
+
+
+const MainForm = styled.div`
+    width:30%;
+    height:80%;
+    border-radius:10px;
+    background-color:rgba(255,255,255, 0.05);
+    padding:1em;
+    box-shadow: 4px 4px 4px rgba(0,0,0,0.3);
+`;
+
+const AppTitle = styled.div`
+    width:100%;
+    text-align: center;
+    color:white;
+    font: 1.7em Montserrat;
+`;
+
+
+const FreqContainer = styled.div`
+    display:flex;
+    flex-direction:row;
+    width:100%;
+`
+
+
+
 function App({}: AppProps) {
 
     return (
-        <h1>Hello World</h1>
+        <MainForm>
+            <AppTitle>WebRTC digital radio</AppTitle>
+            <FreqContainer>
+                <InputField />
+                <div>1Hz</div>
+            </FreqContainer>
+        </MainForm>
     )
 }
 
