c7a927f16eb730b2daa95f21d43992ac362a2b81
Aleksander Hjortkær Sand Evensen (Elev) <aleksandhe@viken.no>
2021-03-23 11:00:16 +0100

    Initialized Project files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+interface AppProps {
+    test?:string
+}
+
+function App({}: AppProps) {
+
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+export default App;
